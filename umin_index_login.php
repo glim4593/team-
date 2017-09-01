990805e7d694cf6661ed84849f605786e24550cf
eavanLee <31268141+eavanLee@users.noreply.github.com>
2017-09-01 14:23:40 +0900

    #25 회원계정 로그인 시에 처리되는 쿼리구문 오류
    #25 회원계정 로그인 시 처리되는 쿼리 구문 오류에 대해 오류 구문 출력이 아닌 오류 메세지가 출력되도록 수정

--- umin_index_login.php
+++ umin_index_login.php
@@ -16,61 +16,74 @@
 
 	$sql = "select * from user where id = '{$id}' and pass = sha2('{$password}' , 0)";
 	$result = mysqli_query($conn, $sql);
-	$row = mysqli_num_rows($result);
-	$arr = mysqli_fetch_assoc($result);
-  mysqli_free_result($result);
-	if($id == '' or $password =='')
+
+	
+	if(!$result)
 	{
 		echo "<script>
-				alert('ID 혹은 PASSWORD를 입력하지 않았습니다.');
+				alert('데이터를 가져오는데 실패했습니다.');
 				location.href='javascript:history.back()';
 				</script>";
-
-
-		mysqli_close($conn);
 		exit;
 	}
-	//print_r($row);
-
-	if($row>0)
+	else
 	{
-		if($arr['active'] == 1)
+		$row = mysqli_num_rows($result);
+		$arr = mysqli_fetch_assoc($result);
+		mysqli_free_result($result);
+		if($id == '' or $password =='')
 		{
-			$_SESSION['id'] = $arr['id'];
-      $_SESSION['host'] = 'menu';
-
-			date_default_timezone_set("Asia/Seoul");
-			$login_time = date('Y-m-d H:i:s' , time());
-			$sql_login = "update user set last_login = '{$login_time}' where id = '{$id}'";
-			$result_login = mysqli_query($conn, $sql_login);
-
 			echo "<script>
-					alert('로그인 되었습니다.');
-					location.href='/board/team-/free_menu.php?page=1';
+					alert('ID 혹은 PASSWORD를 입력하지 않았습니다.');
+					location.href='javascript:history.back()';
 					</script>";
 
-          mysqli_close($conn);
+
+			mysqli_close($conn);
 			exit;
+		}
+		//print_r($row);
+
+		if($row>0)
+		{
+			if($arr['active'] == 1)
+			{
+				$_SESSION['id'] = $arr['id'];
+				$_SESSION['host'] = 'menu';
+
+				date_default_timezone_set("Asia/Seoul");
+				$login_time = date('Y-m-d H:i:s' , time());
+				$sql_login = "update user set last_login = '{$login_time}' where id = '{$id}'";
+				$result_login = mysqli_query($conn, $sql_login);
+
+				echo "<script>
+						alert('로그인 되었습니다.');
+						location.href='/board/team-/free_menu.php?page=1';
+						</script>";
+
+				mysqli_close($conn);
+				exit;
+			}
+			else
+			{
+				echo "<script>
+					alert('휴면 계정입니다 계정 활성화를 하세요');
+					location.href='/board/team-/umin_active.php?id={$id}';
+					</script>"; // 데이터 GET방식으로 id값 전달
+
+				mysqli_close($conn);
+			}
 		}
 		else
 		{
 			echo "<script>
-				alert('휴면 계정입니다 계정 활성화를 하세요');
-				location.href='/board/team-/umin_active.php?id={$id}';
-				</script>"; // 데이터 GET방식으로 id값 전달
+					alert('ID나 PASSWORD가 틀렸습니다.');
+					location.href='javascript:history.back()';
+					</script>";
 
-        mysqli_close($conn);
+			mysqli_close($conn);
+			exit;
 		}
 	}
-	else
-	{
-		echo "<script>
-				alert('ID나 PASSWORD가 틀렸습니다.');
-				location.href='javascript:history.back()';
-				</script>";
 
-        mysqli_close($conn);
-		exit;
-	}
-/*주석 추가*/
 ?>
