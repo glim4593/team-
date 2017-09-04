71ee88c7307eb04706c41d466a348a0f5b010f23
glim4593 <31301447+glim4593@users.noreply.github.com>
2017-09-04 16:14:48 +0900

    권한이 없는 유저 게시글 삭제 방지
    권한 없는 유저는 게시글을 삭제하지 못하도록 수정했습니다.

--- umin_write_del.php
+++ umin_write_del.php
@@ -1,8 +1,23 @@
 <?php
+ session_start();
+
   $conn = mysqli_connect('127.0.0.1', 'root', '1234', 'umin');
   $no = $_GET['no'];
+  $sql = "SELECT * from board where no = {$no}";
+  $result = mysqli_query($conn, $sql);
+  $arr = mysqli_fetch_assoc($result);
+  mysqli_free_result($result);
+  if($arr['writer'] != $_SESSION['id'] and $_SESSION['id'] != 'master')
+  {
+    echo "<script>
+          alert('게시글 삭제 권한이 없습니다.')
+          location.href='javascript:history.back()';
+          </script>";
+          exit;
+  }
+
   $sql = "DELETE FROM board WHERE no = {$no}";
-  //echo $sql;
+  //print_r($sql);
   //exit;
   $result = mysqli_query($conn,$sql);
 
